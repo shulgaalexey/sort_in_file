0dabf3dd843e099acecee9776dbae4bf44d61f3e
shulgaalexey <ShulgaAlexey@gmail.com>
2015-11-11 17:56:10 +0900

    Update-2015-11-11

--- common.h
+++ common.h
@@ -9,30 +9,24 @@ typedef unsigned int DATA_TYPE;
 
 /* The size of the input file */
 const size_t INPUT_SIZE =
-	25;
+	//25;
 	//200;
-
-/* The number of buckets */
-const int BUCKET_COUNT =
-	5;
-	//10;
-
-/* The minimal value of the data */
-const int VAL_MIN = 0;
-
-/* The maximal value of the data */
-const int VAL_MAX =
-	30;
-	//5000;
-
-/* The range of item values in each bucket */
-const int BUCKET_RANGE_SIZE = (VAL_MAX - VAL_MIN) / BUCKET_COUNT;
+	//2000;
+	//50000;
+	//500000;
+	5000000;
 
 /* The maximum number of items, which may be sotred in the memory */
-const size_t MAX_ITEM_COUNT_IN_MEMORY_ALLOWED = 50;
+const size_t MAX_ITEM_COUNT_IN_MEMORY_ALLOWED =
+	//10;
+	//50;
+	//100;
+	1000;
 
 
-const bool __debug_trace = true;
+const bool __debug_trace =
+	//true;
+	false;
 
 #endif /* _COMMON_H_INCLUDED_ */
 
