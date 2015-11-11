5993842ad8877ef78070c9e263540060f3f43d11
shulgaalexey <ShulgaAlexey@gmail.com>
2015-11-11 17:57:07 +0900

    Update-2015-11-11

--- resource_monitor.cpp
+++ resource_monitor.cpp
@@ -4,9 +4,9 @@
 #include "resource_monitor.h"
 #include "bucket.h"
 
-using namespace std;
 
 resource_monitor::resource_monitor()
+	: _cur_items_in_memory(0)
 {
 }
 
@@ -14,11 +14,11 @@ resource_monitor::~resource_monitor()
 {
 }
 
-resource_monitor *resource_monitor::get_ptr()
+/*resource_monitor *resource_monitor::get_ptr()
 {
 	static resource_monitor mon;
 	return &mon;
-}
+}*/
 
 void resource_monitor::register_bucket(bucket *b)
 {
@@ -48,10 +48,30 @@ void resource_monitor::items_released(size_t cnt)
 
 void resource_monitor::flush_buckets()
 {
-	for(map<bucket *, int>::iterator it = _buckets.begin();
+	for(std::map<bucket *, int>::iterator it = _buckets.begin();
 			it != _buckets.end(); ++it) {
 		bucket *b = it->first;
 		if(b && (it->second == 1))
 			b->flush();
 	}
+}
+
+bool resource_monitor::enough_memory_for(size_t cnt)
+{
+	return true;
+	return ((_cur_items_in_memory + cnt)
+		<= MAX_ITEM_COUNT_IN_MEMORY_ALLOWED);
+}
+
+void resource_monitor::use_memory(size_t cnt)
+{
+		_cur_items_in_memory += cnt;
+}
+
+void resource_monitor::release_memory(size_t cnt)
+{
+	if(_cur_items_in_memory <= cnt)
+		_cur_items_in_memory = 0;
+	else
+		_cur_items_in_memory -= cnt;
 }
