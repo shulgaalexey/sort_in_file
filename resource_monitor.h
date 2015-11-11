201dda6accf9e46401f953e428e95b8858bcb448
shulgaalexey <ShulgaAlexey@gmail.com>
2015-11-11 17:57:39 +0900

    Update-2015-11-11

--- resource_monitor.h
+++ resource_monitor.h
@@ -18,13 +18,20 @@ class resource_monitor {
 	public:
 		resource_monitor();
 		virtual ~resource_monitor();
-	public:
-		static resource_monitor *get_ptr();
+		/* The singletone is nice, but it conflicts with the conception
+		 * of cascading file sort when each bucket file can be sorted
+		 * with sort_file algorithm
+		 */
+	/*public:
+		static resource_monitor *get_ptr();*/
 	public:
 		void register_bucket(bucket *b);
 		void unregister_bucket(bucket *b);
 		void items_added(size_t cnt);
 		void items_released(size_t cnt);
+		void use_memory(size_t cnt);
+		void release_memory(size_t cnt);
+		bool enough_memory_for(size_t cnt);
 	protected:
 		void flush_buckets();
 };
